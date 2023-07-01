0907243c82f1e00e1d2154c9cc8b341b4043cdd1
amitansu1024 <amitanshurajneti@gmail.com>
2023-07-01 08:54:55 +0530

    movement

---
+++ Assets/Scripts/PlayerMovementHandler.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody), typeof(BoxCollider))]
+public class PlayerMovementHandler : MonoBehaviour
+{
+    [SerializeField] private FixedJoystick _movementJoystick;
+    [SerializeField] private FixedJoystick _rotationJoystick;
+    private float _movementSpeed;
+    private float _rotationSpeed;
+    private Rigidbody _rigidBody;
+
+    void Awake() {
+        _rigidBody = GetComponent<Rigidbody>();
+        _movementSpeed = 0.1f;
+        _rotationSpeed = 0.6f;
+    }
+
+    void FixedUpdate() {
+        if (_rigidBody.velocity.magnitude < 1.0f) {
+            _rigidBody.velocity += transform.forward * _movementJoystick.Vertical * _movementSpeed;
+            _rigidBody.velocity += transform.right * _movementJoystick.Horizontal * _movementSpeed;
+        }
+
+        _rigidBody.transform.eulerAngles += 
+            new Vector3(- _rotationJoystick.Vertical * _rotationSpeed, _rotationJoystick.Horizontal * _rotationSpeed, 0);
+    } 
+}
